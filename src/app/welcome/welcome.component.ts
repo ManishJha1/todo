59781bf48bfa03ee45aa3d0efb418e477a1a63ab
ManishJha1 <mjha9723@gmail.com>
2022-09-01 23:10:28 +0530

    server connection with ui. Upto Delete functionality.

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { WelcomeDataService } from '../service/data/welcome-data.service';
 
 @Component({
   selector: 'app-welcome',
@@ -12,14 +13,54 @@ export class WelcomeComponent implements OnInit {
   title = 'welcome page title'
 
   message = 'Some welcome message';
+  welcomeMessageFromService!: string;
   name = '';
 
-  constructor(private route : ActivatedRoute) { }//dependency injection
+  constructor(
+    private route : ActivatedRoute,
+    private welcomeData : WelcomeDataService
+    ) { }//dependency injection
 
   ngOnInit(): void {
     console.log(this.message);
     //console.log(this.route.snapshot.params['params']);//will provide snapshot(print parms) of params
     this.name = this.route.snapshot.params['params'];//activated route are used to print the incoming parmeter along with the routes and that route will be defined in routing module
+  }
+
+  getWelcomeMessage(){
+    //console.log(this.welcomeData.executeHelloWorldBeanService());//will show object
+    // this.welcomeData.executeHelloWorldBeanService()// if not subscribed will not work as observables are invoked when subscribed
+    this.welcomeData.executeHelloWorldBeanService().subscribe(
+      response => this.handleSuccessfulResponse(response),//it directs to handleSuccessfulResponse function
+      error => this.handleErrorResponse(error)//it handles common error response by directing it to handleErrorResponse
+    );
+    console.log('last line of getMessage');
+    //console.log('Get welcome message');
+  }
+
+  getWelcomeMessageWithParameter(){
+    //console.log(this.welcomeData.executeHelloWorldBeanService());//will show object
+    // this.welcomeData.executeHelloWorldBeanService()// if not subscribed will not work as observables are invoked when subscribed
+    this.welcomeData.executeHelloWorldServiceWithParameter(this.name).subscribe(
+      response => this.handleSuccessfulResponse(response),//it directs to handleSuccessfulResponse function
+      error => this.handleErrorResponse(error)//it handles common error response by directing it to handleErrorResponse
+    );
+    console.log('last line of getMessage');
+    //console.log('Get welcome message');
+  }
+
+  //used to check response object and specific message.
+  handleSuccessfulResponse(response: any){
+    this.welcomeMessageFromService = response.message
+    // console.log(response);
+    // console.log(response.message);
+  }
+
+  handleErrorResponse(error: any){
+    // console.log(error);
+    // console.log(error.error);
+    // console.log(error.error.message);
+    this.welcomeMessageFromService = error.error.message;
   }
 
 }
